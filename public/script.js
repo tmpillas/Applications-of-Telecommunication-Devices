268cff8d48bd28e15ba62d4528fd506f45a3b58c
tmpillas <tmpillas@ece.auth.gr>
2025-05-20 14:48:32 +0300

    Login page working, added styling

---
+++ public/script.js
@@ -0,0 +1,12 @@
+function login(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    if (username === "admin" && password === "1234") {
+      window.location.href = "index.html";
+    } else {
+      alert("Λάθος στοιχεία σύνδεσης. Προσπαθήστε ξανά.");
+    }
+  }
+  
