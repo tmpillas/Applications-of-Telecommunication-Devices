c73f8e4754bedacc20ba97c06b305134c16556ed
FotiosPaparisvas <fpaparisv@ece.auth.gr>
2025-05-27 14:00:35 +0300

    Added proximity sensor code-Fotis

--- Code/AchileasBillas/src/main.cpp
+++ Code/AchileasBillas/src/main.cpp
@@ -3,10 +3,13 @@
 #include <RF22.h>
 #include <RF22Router.h>
 
+#define pingPin  A1
 #define MY_ADDRESS 67 // define my unique address
 #define DESTINATION_ADDRESS_1 66 // define who I can talk to
 //#define DESTINATION_ADDRESS_2 3 // define who I can talk to
 
+
+long microsecondsToCentimeters(long microseconds);
 RF22Router rf22(MY_ADDRESS); // initiate the class to talk to my radio with MY_ADDRESS
 int number_of_bytes=0; // will be needed to measure bytes of message
 float throughput=0; // will be needed for measuring throughput
@@ -29,15 +32,16 @@ char hexaKeys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {9, 8, 7, 6};
-byte colPins[COLS] = {5, 4, 1, 0};
+byte rowPins[ROWS] = {9, A2, 7, 6};
+byte colPins[COLS] = {5, 4, 3, A3};
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 String inputCode = "";
 const String password = "1220";
-bool buzzerflag = false;
+bool buzzerflag = false,send;
 int attempts = 3;
+long duration, cm;
 
 void setup() {
 Serial.begin(9600); // to be able to view the results in the computer's monitor
@@ -70,7 +74,23 @@ Serial.begin(9600); // to be able to view the results in the computer's monitor
 }
 
 void loop() 
-{
+{ 
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+
+
 	char customKey = customKeypad.getKey();
 
   if (customKey &&  !buzzerflag) {
@@ -117,48 +137,56 @@ void loop()
   int sensorVal = 12; // measure something
  //Serial.print("My measurement is: ");
   //Serial.println(sensorVal); // and show it on Serial
+  if(send){
+  // the following variables are used in order to transform my integer measured value into a uint8_t variable, which is proper for my radio
+    char data_read[RF22_ROUTER_MAX_MESSAGE_LEN];
+    uint8_t data_send[RF22_ROUTER_MAX_MESSAGE_LEN];
+    memset(data_read, '\0', RF22_ROUTER_MAX_MESSAGE_LEN);
+    memset(data_send, '\0', RF22_ROUTER_MAX_MESSAGE_LEN);    
+    sprintf(data_read, "%d", sensorVal); // I'm copying the measurement sensorVal into variable data_read
+    data_read[RF22_ROUTER_MAX_MESSAGE_LEN - 1] = '\0'; 
+    memcpy(data_send, data_read, RF22_ROUTER_MAX_MESSAGE_LEN); // now I'm copying data_read to data_send
+    number_of_bytes=sizeof(data_send); // I'm counting the number of bytes of my message
+    Serial.print("Number of Bytes= ");
+    Serial.println(number_of_bytes);  // and show the result on my monitor
 
-// the following variables are used in order to transform my integer measured value into a uint8_t variable, which is proper for my radio
-  char data_read[RF22_ROUTER_MAX_MESSAGE_LEN];
-  uint8_t data_send[RF22_ROUTER_MAX_MESSAGE_LEN];
-  memset(data_read, '\0', RF22_ROUTER_MAX_MESSAGE_LEN);
-  memset(data_send, '\0', RF22_ROUTER_MAX_MESSAGE_LEN);    
-  sprintf(data_read, "%d", sensorVal); // I'm copying the measurement sensorVal into variable data_read
-  data_read[RF22_ROUTER_MAX_MESSAGE_LEN - 1] = '\0'; 
-  memcpy(data_send, data_read, RF22_ROUTER_MAX_MESSAGE_LEN); // now I'm copying data_read to data_send
-  number_of_bytes=sizeof(data_send); // I'm counting the number of bytes of my message
-  Serial.print("Number of Bytes= ");
-  Serial.println(number_of_bytes);  // and show the result on my monitor
+    // just demonstrating that the string I will send, after those transformation from integer to char and back remains the same
+    int sensorVal2=0;
+    sensorVal2=atoi(data_read);
+    Serial.print("The string I'm ready to send is= ");
+    Serial.println(sensorVal2);
+    if (rf22.sendtoWait(data_send, sizeof(data_send), DESTINATION_ADDRESS_1) != RF22_ROUTER_ERROR_NONE) // I'm sending the data in variable data_send to DESTINATION_ADDRESS_1... cross fingers
+    {
+      Serial.println("sendtoWait failed"); // for some reason I have failed
+    }
+    else
+    {
+      counter=counter+1;
+      rssi = rf22.rssiRead();
+      Pr=((float)rssi-230.0)/1.8;
+      Serial.print("RSSI= ");
+      Serial.print(Pr);
+      Serial.println(" dBm");
 
-  // just demonstrating that the string I will send, after those transformation from integer to char and back remains the same
-  int sensorVal2=0;
-  sensorVal2=atoi(data_read);
-  Serial.print("The string I'm ready to send is= ");
-  Serial.println(sensorVal2);
-  if (rf22.sendtoWait(data_send, sizeof(data_send), DESTINATION_ADDRESS_1) != RF22_ROUTER_ERROR_NONE) // I'm sending the data in variable data_send to DESTINATION_ADDRESS_1... cross fingers
-  {
-    Serial.println("sendtoWait failed"); // for some reason I have failed
+
+      Serial.println("sendtoWait Successful"); // I have received an acknowledgement from DESTINATION_ADDRESS_1. Data have been delivered!
+    }
+    final_time=millis();
+    throughput=(float)counter*number_of_bytes*1000.0/(final_time-initial_time); // *1000 is because time is measured in ms. This is not the communication throughput, but rather each measurement-circle throughput.
+    Serial.print("Throughput=");
+    Serial.print(throughput);
+    Serial.println("Bytes/s");
+    Serial.print("Initial time= ");  
+    Serial.print(initial_time);
+    Serial.print("     Final time= ");  
+    Serial.println(final_time);
   }
-  else
-  {
-    counter=counter+1;
-    rssi = rf22.rssiRead();
-    Pr=((float)rssi-230.0)/1.8;
-    Serial.print("RSSI= ");
-    Serial.print(Pr);
-    Serial.println(" dBm");
+}
 
 
-    Serial.println("sendtoWait Successful"); // I have received an acknowledgement from DESTINATION_ADDRESS_1. Data have been delivered!
-  }
-  final_time=millis();
-  throughput=(float)counter*number_of_bytes*1000.0/(final_time-initial_time); // *1000 is because time is measured in ms. This is not the communication throughput, but rather each measurement-circle throughput.
-  Serial.print("Throughput=");
-  Serial.print(throughput);
-  Serial.println("Bytes/s");
-  Serial.print("Initial time= ");  
-  Serial.print(initial_time);
-  Serial.print("     Final time= ");  
-  Serial.println(final_time);
-
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
 }
